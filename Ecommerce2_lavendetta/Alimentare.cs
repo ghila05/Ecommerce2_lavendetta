aa96702c3e755b6af5d781131ccfa1f12a29a99c
ghilardi.19320 <ghilardi.19320@LABI1-6PC12>
2023-01-30 09:36:56 +0100

    implementazioni classi

---
+++ Ecommerce2_lavendetta/Alimentare.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce2_lavendetta
+{
+    public class Alimentare: Prodotto
+    {
+        string[] ingredienti = new string[10];
+        string date;
+
+        public string Date
+        {
+            get
+            {
+                return date;
+            }
+            private set
+            {
+                if (value != "")
+                {
+                    date = value;
+                }
+            }
+        }
+
+        public string[] Ingredienti
+        {
+            get
+            {
+                return ingredienti;
+            }
+            private set
+            {
+                if (value != null)
+                {
+                    ingredienti = value;
+                }
+            }
+        }
+
+        public bool ControllaScadenza(DateTime scadenza)
+        {
+            return scadenza < DateTime.Now;
+        }
+      
+        public Alimentare(string[] ingredienti, string date)
+        {
+          
+        }
+    }
+}
---
+++ Ecommerce2_lavendetta/Elettronica.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce2_lavendetta
+{
+    public class Elettronica: Prodotto
+    {
+        string _modello;
+
+        public string Modello
+        {
+            get
+            {
+                return _modello;
+            }
+            private set
+            {
+                if (value != "")
+                {
+                    _modello = value;
+                }
+            }
+        }
+
+        public Elettronica(string id, string nome, string prod, string descr, float prezzo, string modello): base (id,nome,prod,descr)
+        {
+            Modello = modello;
+        }
+        public Elettronica() : this(null,null,null,null,0,null)
+        {
+
+        }
+
+        public double getSconto(float price)
+        {
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return price * 0.95;
+            }
+            else
+            {
+                return price;
+            }
+        }
+
+    }
+}
